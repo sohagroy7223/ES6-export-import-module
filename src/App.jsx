3b74fb7c11d3914c8b73a1afee8ef7eb98a3338f
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-01 12:38:17 +0600

    addad if decler a big function name we can change it and give short name

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import { add, diff, malt } from "./utils/math/math";
+import { add, diff, divide as div, malt } from "./utils/math/math";
 function App() {
   const sum = add(10, 20);
   const deferent = diff(20, 10);
-  const gunfol = malt(2, 5);
-  console.log(sum, deferent, gunfol);
+  const goonfol = malt(5, 5);
+  const divide = 12 / 4;
+  const something = div(20, 4);
+  console.log(sum, deferent, goonfol, something);
   return (
     <>
       <h1>Vite + React</h1>
--- src/utils/math/math.js
+++ src/utils/math/math.js
@@ -1,5 +1,6 @@
 const add = (a, b) => a + b;
 const diff = (a, b) => a - b;
 const multiplication = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
-export { add, diff, multiplication as malt };
+export { add, diff, multiplication as malt, divide };

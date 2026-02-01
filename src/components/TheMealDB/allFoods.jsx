4aebecd95a17fd18655b760b23f73fa4ec14f928
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-01 15:04:18 +0600

    addad card css style and btn togol

--- src/components/TheMealDB/allFoods.jsx
+++ src/components/TheMealDB/allFoods.jsx
@@ -3,7 +3,7 @@ import "./allFood.css";
 import Food from "./food/food";
 const AllFoods = ({ allFoods }) => {
   const foods = use(allFoods);
-  console.log(foods);
+  // console.log(foods);
   return (
     <div>
       <h3>all TheMealDB foods is here: {foods.meals.length}</h3>
--- src/components/TheMealDB/food/food.jsx
+++ src/components/TheMealDB/food/food.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./food.css";
 const Food = ({ food }) => {
-  console.log(food);
+  const [buy, setBuy] = useState(false);
+  const handelBuy = () => {
+    setBuy(!buy);
+  };
+
+  //   console.log(food);
   return (
-    <div className="card">
+    <div className={`card ${buy && "clickBtn"}`}>
       <h3>name: {food.strMeal}</h3>
       <img className="img" src={food.strMealThumb} alt="" />
       <h4>strArea: {food.strArea}</h4>
+      <button onClick={handelBuy}>{buy ? "cancel" : "buy"}</button>
     </div>
   );
 };

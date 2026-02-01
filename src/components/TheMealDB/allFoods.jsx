44f74e4520ad98c938b426dcbae8b83d74d15ee7
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-01 14:45:00 +0600

    addad food thubmile , name an d survice area

--- src/components/TheMealDB/allFoods.jsx
+++ src/components/TheMealDB/allFoods.jsx
@@ -1,11 +1,17 @@
 import React, { use } from "react";
-// import Food from "./food/food";
+import "./allFood.css";
+import Food from "./food/food";
 const AllFoods = ({ allFoods }) => {
   const foods = use(allFoods);
   console.log(foods);
   return (
     <div>
       <h3>all TheMealDB foods is here: {foods.meals.length}</h3>
+      <div className="foods">
+        {foods.meals.map((food) => (
+          <Food food={food}></Food>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/TheMealDB/food/food.jsx
+++ src/components/TheMealDB/food/food.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import "./food.css";
+const Food = ({ food }) => {
+  console.log(food);
+  return (
+    <div className="card">
+      <h3>name: {food.strMeal}</h3>
+      <img className="img" src={food.strMealThumb} alt="" />
+      <h4>strArea: {food.strArea}</h4>
+    </div>
+  );
+};
 
+export default Food;

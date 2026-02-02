b18763ec306cb68257c56b55bdf66ba8240c7904
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-02 12:18:14 +0600

    addad order count in this file

--- src/components/TheMealDB/allFoods.jsx
+++ src/components/TheMealDB/allFoods.jsx
@@ -3,17 +3,22 @@ import "./allFood.css";
 import Food from "./food/food";
 const AllFoods = ({ allFoods }) => {
   const foods = use(allFoods);
-  const [addCard, setAddCerd] = useState([]);
-  const CardAdd = () => {
-    console.log("add card");
+
+  const [ordered, setOrdered] = useState(0);
+
+  const handelOrder = () => {
+    const allOrdered = ordered + 1;
+    setOrdered(allOrdered);
   };
+
   // console.log(foods);
   return (
     <div>
       <h3>all TheMealDB foods is here: {foods.meals.length}</h3>
+      <h3>buy food: {ordered} </h3>
       <div className="foods">
         {foods.meals.map((food) => (
-          <Food card={CardAdd} food={food}></Food>
+          <Food handelOrder={handelOrder} food={food}></Food>
         ))}
       </div>
     </div>
--- src/components/TheMealDB/food/food.jsx
+++ src/components/TheMealDB/food/food.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./food.css";
-const Food = ({ food, card }) => {
+const Food = ({ food, handelOrder }) => {
   const [buy, setBuy] = useState(false);
   const handelBuy = () => {
     setBuy(!buy);
@@ -13,7 +13,7 @@ const Food = ({ food, card }) => {
       <img className="img" src={food.strMealThumb} alt="" />
       <h4>strArea: {food.strArea}</h4>
       <button onClick={handelBuy}>{buy ? "cancel" : "buy"}</button>
-      <button onClick={card}>add to card</button>
+      <button onClick={handelOrder}>add card</button>
     </div>
   );
 };
